9ab1aaeee565bc7f5270daa8bcd5f7308f80ec7e
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2021-01-26 05:37:40 -0500

    Made flexible

--- PLSDA-MultiplePlugin.R
+++ PLSDA-MultiplePlugin.R
@@ -1,3 +1,7 @@
+
+dyn.load(paste("RPluMA", .Platform$dynlib.ext, sep=""))
+source("RPluMA.R")
+
 require(graphics)
 library("caret")
 library("spls")
@@ -5,73 +9,61 @@ library(mlbench)
 
 input <- function(inputfile) {
   parameters <<- read.table(inputfile, as.is=T);
+pfix = prefix()
+  if (length(pfix) != 0) {
+     pfix <- paste(pfix, "/", sep="")
+  }
   rownames(parameters) <<- parameters[,1];
   print("READING INPUT FILES...");
-  t1 <<- read.table(toString(parameters["training",2]), sep = "\t", header =FALSE, stringsAsFactors=FALSE)#, nrow=20000)
-  t2 <<- read.table(toString(parameters["clinical",2]), sep="\t", header = TRUE,  stringsAsFactors=FALSE)
+  training_set <<- read.table(paste(pfix, toString(parameters["training",2]), sep=""), sep = "\t", header =FALSE, stringsAsFactors=FALSE)#, nrow=20000)
+  test_set <<- read.table(paste(pfix, toString(parameters["clinical",2]), sep=""), sep="\t", header = TRUE,  stringsAsFactors=FALSE)
+  joincol <<- toString(parameters["joinby", 2])
+  id <<- toString(parameters["id", 2])
+  xcoor <<- toString(parameters["x", 2])
+  classcol <<- toString(parameters["classcol", 2])
   print("DONE");
-  #prefix <<- toString(parameters["prefix", 2]);
-#t1 <- read.table("ViralChallenge_training_EXPRESSION_RMA.tsv", sep = "\t", header =FALSE, stringsAsFactors=FALSE)
-#t2 <- read.table("ViralChallenge_training_CLINICAL.tsv", sep="\t", header = TRUE,  stringsAsFactors=FALSE)
-
-
-#t1 <- as.data.frame(t(t1), stringsAsFactors=FALSE)
-#colnames(t1)[1] = "CEL"
-# rownames(t1) <- substring(rownames(t1), 2, length(rownames(t1)))
-# write.table(t1, file = "shortRMA.csv", sep = ",", row.names = FALSE, col.names = TRUE)
-#x <- as.data.frame(merge(t1, t2, by ="CEL", stringsAsFactors=FALSE))
-
-#studyID = unique(x$STUDYID)
 }
 
 run <- function() {
-
-#rm(list=ls(all=TRUE))
-#setwd("appData/Res_Challenge/Data")
-#t1 = read.table("ViralChallenge_training_EXPRESSION_RMA.tsv", sep = "\t", header = FALSE, stringsAsFactors=FALSE)
-#t2 <- read.table("ViralChallenge_training_CLINICAL.tsv", sep="\t", header = TRUE,  stringsAsFactors=FALSE)
-
-t2[t2$STUDYID == "DEE4X H1N1",]$STUDYID="H1N1"
-t2[t2$STUDYID == "DEE3 H1N1",]$STUDYID="H1N1"
-t2[t2$STUDYID == "DEE2 H3N2",]$STUDYID="H3N2"
-t2[t2$STUDYID == "DEE5 H3N2",]$STUDYID="H3N2"
-t2[t2$STUDYID == "Rhinovirus Duke",]$STUDYID="Rhinovirus"
-t2[t2$STUDYID == "Rhinovirus UVA",]$STUDYID="Rhinovirus"
-
-t1 <<- as.data.frame(t(t1), stringsAsFactors=FALSE)
-colnames(t1)[1] <<- "CEL"
-# rownames(t1) <- substring(rownames(t1), 2, length(rownames(t1)))
-# write.table(t1, file = "shortRMA.csv", sep = ",", row.names = FALSE, col.names = TRUE)
-x <<- merge(t1, t2, by ="CEL", stringsAsFactors=FALSE)
-
-studyID <<- unique(x$STUDYID)
+# transpose the matrix
+training_set <<- as.data.frame(t(training_set), stringsAsFactors=FALSE)
+train_set_size <<- ncol(training_set)
+class_index <<- grep(classcol, colnames(test_set))
+# provide a label for the first column
+colnames(training_set)[1] <<- joincol
+# merge test and training sets
+merged_set <<- merge(training_set, test_set, by =joincol, stringsAsFactors=FALSE)
+# obtain all unique STUDYIDs
+studyID <<- unique(as.character(unlist(merged_set[id])))
+#studyID <<- unique(merged_set$STUDYID)
 }
 
 output <- function(outputfile) {
+# All viruses
 for(virus in studyID){
-
-  v1 = x[x[,"STUDYID"]==virus,]
-  times = unique(v1$TIMEHOURS)
+  # Pick only the samples for this virus
+  virus_set = merged_set[merged_set[,id]==virus,]
+  # Get the different times
+  #times = unique(virus_set$TIMEHOURS)
+  times = unique(as.numeric(unlist(virus_set[xcoor])))
+  # For every time
   for(t in times){
-    t.x = data.matrix(v1[v1[,"TIMEHOURS"]==t,2:22278])
-    t.y = as.factor(v1[v1[,"TIMEHOURS"]==t,22286])
-
-    # t.x = data.matrix(v1[v1[,"TIMEHOURS"]==t,2:100])
-    # t.y = as.factor(v1[v1[,"TIMEHOURS"]==t,108])
+    # Expression data.  Column 1 is CEL, remove.  22278 is hardcoded, only works
+    # for this case.  Where the expression data stops.
+    #vv = virus_set[virus_set[,"TIMEHOURS"]==t, 2:22278]
+    vv = virus_set[virus_set[,xcoor]==t, 2:train_set_size]
+    t.x = data.matrix(matrix(as.numeric(unlist(vv)),nr=nrow(vv)))
+    # Column SYMPTOMATIC_SC2, hardcoded.
+    #t.y = as.factor(virus_set[virus_set[,"TIMEHOURS"]==t,22286])
+    t.y = as.factor(virus_set[virus_set[,xcoor]==t,train_set_size+class_index])
     my_pls1 = plsda(t.x, t.y)
 
     res = order(varImp(my_pls1), decreasing = TRUE)[1:1000]
-
-    # resCon = sapply(res, function(x) varImp(my_pls1)[1][[1]][x] > 0.01)
-    # fin = res[resCon]
     imp = varImp(my_pls1)[1][[1]][c(res)]
 
 
     df = data.frame(res, imp)
     write.csv(df, file = paste(outputfile,virus,"_",t,".csv", sep=""), row.names = FALSE)
-    #hist(varImp(my_pls1)[1][[1]][c(res)])
-    # break;
   }
-  # break;
 }
 }
